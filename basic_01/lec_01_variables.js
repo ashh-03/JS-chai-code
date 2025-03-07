a47266ce0ad5eef8b9b7512b8c0810381dae3557
ashh-03 <ashishtiwary2003@gmail.com>
2025-03-07 04:04:39 +0000

    variables

---
+++ basic_01/lec_01_variables.js
@@ -0,0 +1,8 @@
+ const accountId = 1234
+ var accountEmail = "@ashh"
+ let accountPass ="12222"
+ hello ="this is not good"
+ let boss;
+
+// console.log(accountId);
+console.table([accountId , accountEmail , accountPass , hello , boss]);
